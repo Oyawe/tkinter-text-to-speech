97dc52daa4f0b4b26a2adf74f0c7551f4ced5d58
Oyawe <oyawegoodness@gmail.com>
2023-09-23 07:48:36 -0700

    first commit

---
+++ text-to-speech.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Combobox
+import pyttsx3
+import os
+
+root = Tk()
+root.title("Text to speech")
+root.geometry("900x450")
+root.resizable(False, False)
+
+
+root.mainloop()
